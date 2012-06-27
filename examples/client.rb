ab1c73a5a74d91aeb26529a9aee8ff600b5eaf23
Aaron Quint <aaron@quirkey.com>
2012-06-27 14:14:02 +0100

    Do it 100 times

--- examples/client.rb
+++ examples/client.rb
@@ -1,8 +1,10 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), '../reqrep.rb')
 
+100.times do
 puts "Reversing"
 start = Time.now
 reply = Reqrep.request_and_wait_for_reply(:reverse, {}, "Sally sells seashells")
 puts reply.inspect
 total = Time.now.to_f - start.to_f
 puts "Took #{total * 1000}ms"
+end
